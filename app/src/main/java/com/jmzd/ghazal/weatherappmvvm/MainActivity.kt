8896b904d1500c8b9576424b1f2ad2d0b73fcab6
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-18 19:47:49 +0430

    #420 MainActivity

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.jmzd.ghazal.weatherappmvvm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
+import com.jmzd.ghazal.weatherappmvvm.databinding.ActivityMainBinding
+import com.jmzd.ghazal.weatherappmvvm.utils.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity() {
+
+    //Binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        //Transparent status bar
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
