f0defe3058bc2e38a5fe9764a762a2b696ebad28
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-19 18:42:48 +0430

    #425 currentWeather : repository

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/data/repository/MainRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/data/repository/MainRepository.kt
@@ -2,10 +2,15 @@ package com.jmzd.ghazal.weatherappmvvm.data.repository
 
 import com.jmzd.ghazal.weatherappmvvm.data.database.CitiesDao
 import com.jmzd.ghazal.weatherappmvvm.data.network.ApiServices
+import com.jmzd.ghazal.weatherappmvvm.utils.FA
+import com.jmzd.ghazal.weatherappmvvm.utils.METRIC
 import javax.inject.Inject
 
-class MainRepository @Inject constructor(private val dao: CitiesDao) {
+class MainRepository @Inject constructor(private val dao: CitiesDao , private val api : ApiServices) {
     //Database
     fun getCities() = dao.loadCities()
+
+    //Api
+    suspend fun getCurrentWeather(lat: Double, lon: Double) = api.getCurrentWeather(lat, lon, FA, METRIC)
 
 }
