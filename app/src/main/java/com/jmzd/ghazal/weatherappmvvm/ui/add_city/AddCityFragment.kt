42a0798844672a046459fd814e104c081171d2e9
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-19 13:32:41 +0430

    #421 add city : fragment created

---
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/add_city/AddCityFragment.kt
@@ -0,0 +1,13 @@
+package com.jmzd.ghazal.weatherappmvvm.ui.add_city
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jmzd.ghazal.weatherappmvvm.R
+
+class AddCityFragment : Fragment() {
+
+
+}
