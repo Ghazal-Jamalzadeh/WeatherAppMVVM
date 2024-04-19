85dd0dadc9643a3e974f391678c61e68e96b919f
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-19 14:02:21 +0430

    #422 add city : api services

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/data/network/ApiServices.kt
@@ -1,4 +1,13 @@
 package com.jmzd.ghazal.weatherappmvvm.data.network
 
+import com.jmzd.ghazal.weatherappmvvm.data.model.add_city.ResponseCitiesList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiServices {
+
+    @GET("geo/1.0/direct")
+    suspend fun getCitiesList(@Query("q") q: String, @Query("limit") limit: Int): Response<ResponseCitiesList>
+
 }
