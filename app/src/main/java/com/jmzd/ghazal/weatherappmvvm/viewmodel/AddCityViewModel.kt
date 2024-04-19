cab458368bd34bcbd9999e82095ff31213266e61
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-19 15:56:01 +0430

    #423 saveSelectedCity : viewModel

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/viewmodel/AddCityViewModel.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/viewmodel/AddCityViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jmzd.ghazal.weatherappmvvm.data.database.CitiesDao
 import com.jmzd.ghazal.weatherappmvvm.data.database.CitiesEntity
 import com.jmzd.ghazal.weatherappmvvm.data.model.add_city.ResponseCitiesList
 import com.jmzd.ghazal.weatherappmvvm.data.repository.AddCityRepository
@@ -16,16 +17,21 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class AddCityViewModel @Inject constructor(private val repository: AddCityRepository) : ViewModel() {
+class AddCityViewModel @Inject constructor(private val repository: AddCityRepository , private val dao : CitiesDao) : ViewModel() {
     //Search Cities
     private val _searchedCitiesListLiveData = MutableLiveData<NetworkRequest<ResponseCitiesList>>()
     val searchedcitiesListLiveData: LiveData<NetworkRequest<ResponseCitiesList>> = _searchedCitiesListLiveData
 
-    //--- api call ---//
+    //--- api call ---//,
     fun searchCitiesList(search : String) = viewModelScope.launch {
         _searchedCitiesListLiveData.value = NetworkRequest.Loading()
         val response = repository.searchCitiesList(search)
         _searchedCitiesListLiveData.value = NetworkResponse(response).generateResponse()
+    }
+
+    //Save city
+    fun saveCity(entity: CitiesEntity) = viewModelScope.launch {
+        dao.saveCity(entity)
     }
 
 }
