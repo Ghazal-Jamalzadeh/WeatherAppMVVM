4879199f0b3d3de9a00f6317dbbe93f5cbb28a67
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-19 17:04:46 +0430

    #423 saveSelectedCity : event bus

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/add_city/AddCityFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/add_city/AddCityFragment.kt
@@ -5,17 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jmzd.ghazal.weatherappmvvm.R
 import com.jmzd.ghazal.weatherappmvvm.data.database.CitiesEntity
 import com.jmzd.ghazal.weatherappmvvm.data.model.add_city.ResponseCitiesList.ResponseCitiesListItem
 import com.jmzd.ghazal.weatherappmvvm.databinding.FragmentAddCityBinding
 import com.jmzd.ghazal.weatherappmvvm.utils.base.BaseBottomSheetFragment
+import com.jmzd.ghazal.weatherappmvvm.utils.events.EventBus
+import com.jmzd.ghazal.weatherappmvvm.utils.events.Events
 import com.jmzd.ghazal.weatherappmvvm.utils.network.NetworkRequest
 import com.jmzd.ghazal.weatherappmvvm.utils.setupRecyclerview
 import com.jmzd.ghazal.weatherappmvvm.utils.showSnackBar
 import com.jmzd.ghazal.weatherappmvvm.viewmodel.AddCityViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -88,20 +92,20 @@ class AddCityFragment : BaseBottomSheetFragment<FragmentAddCityBinding>(){
         //Click
         citiesAdapter.setOnItemClickListener {
             //Set data into entity
-//            cityEntity.lat = it.lat
-//            cityEntity.lon = it.lon
-//            if (it.localNames?.fa != null)
-//                cityEntity.name = it.localNames.fa
-//            else
-//                cityEntity.name = it.name
+            cityEntity.lat = it.lat
+            cityEntity.lon = it.lon
+            if (it.localNames?.fa != null)
+                cityEntity.name = it.localNames.fa
+            else
+                cityEntity.name = it.name
             //Save city
-//            viewModel.saveCity(cityEntity)
+            viewModel.saveCity(cityEntity)
             //Update event
-//            lifecycleScope.launch {
-//                EventBus.publish(Events.OnUpdateWeather(cityEntity.name, it.lat, it.lon))
-//            }
+            lifecycleScope.launch {
+                EventBus.publish(Events.OnUpdateWeather(cityEntity.name, it.lat, it.lon))
+            }
             //Close dialog
-//            this@AddCityFragment.dismiss()
+            this@AddCityFragment.dismiss()
         }
     }
 
--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/main/MainFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/main/MainFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.jmzd.ghazal.weatherappmvvm.R
 import com.jmzd.ghazal.weatherappmvvm.databinding.FragmentMainBinding
 import com.jmzd.ghazal.weatherappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.weatherappmvvm.utils.events.EventBus
+import com.jmzd.ghazal.weatherappmvvm.utils.events.Events
 import com.jmzd.ghazal.weatherappmvvm.utils.onceObserve
 import com.jmzd.ghazal.weatherappmvvm.utils.setStatusBarIconsColor
 import com.jmzd.ghazal.weatherappmvvm.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainFragment : BaseFragment<FragmentMainBinding>() {
@@ -36,6 +40,14 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
 
         //observers
         loadCitiesData()
+
+        //Event
+        lifecycleScope.launch {
+            EventBus.subscribe<Events.OnUpdateWeather> {
+//                viewModel.callCurrentWeatherApi(it.lat!!, it.lon!!)
+//                viewModel.callForecastApi(it.lat, it.lon)
+            }
+        }
     }
 
     private fun loadCitiesData() {
