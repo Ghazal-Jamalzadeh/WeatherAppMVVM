aa57362d7a0ae0ef5d7ffbc40625bad644851449
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-25 18:14:27 +0430

    #427 Forecast : api services

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/data/network/ApiServices.kt
@@ -2,6 +2,7 @@ package com.jmzd.ghazal.weatherappmvvm.data.network
 
 import com.jmzd.ghazal.weatherappmvvm.data.model.add_city.ResponseCitiesList
 import com.jmzd.ghazal.weatherappmvvm.data.model.main.ResponseCurrentWeather
+import com.jmzd.ghazal.weatherappmvvm.data.model.main.ResponseForecast
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,4 +16,9 @@ interface ApiServices {
     suspend fun getCurrentWeather(
         @Query("lat") lat: Double, @Query("lon") lon: Double, @Query("lang") lang: String, @Query("units") units: String,
     ): Response<ResponseCurrentWeather>
+
+    @GET("data/2.5/forecast")
+    suspend fun getForecast(
+        @Query("lat") lat: Double, @Query("lon") lon: Double, @Query("lang") lang: String, @Query("units") units: String,
+    ): Response<ResponseForecast>
 }
