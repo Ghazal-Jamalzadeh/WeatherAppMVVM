0475b501071901e8aa05a9955b215a4dc1429e46
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-25 19:25:33 +0430

    #428 InfoBottomSheet : show weather info

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/info/InfoFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/info/InfoFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.navigation.fragment.navArgs
+import com.jmzd.ghazal.weatherappmvvm.R
 import com.jmzd.ghazal.weatherappmvvm.databinding.FragmentInfoBinding
+import com.jmzd.ghazal.weatherappmvvm.utils.BASE_URL_IMAGE
+import com.jmzd.ghazal.weatherappmvvm.utils.PNG_IMAGE
 import com.jmzd.ghazal.weatherappmvvm.utils.base.BaseBottomSheetFragment
+import com.jmzd.ghazal.weatherappmvvm.utils.loadImage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,6 +29,34 @@ class InfoFragment : BaseBottomSheetFragment<FragmentInfoBinding>() {
         binding.apply {
             //Args
             args.data.let { data ->
+
+                //Weather
+                data.weather?.let { weather ->
+                    if (weather.isNotEmpty()) {
+                        weather[0]?.let {
+                            //Info
+                            infoTxt.text = it.description
+                            //Image
+                            val image = "$BASE_URL_IMAGE${it.icon}$PNG_IMAGE"
+                            iconImg.loadImage(image)
+                        }
+                    }
+                }
+                //Main
+                data.main?.let { main ->
+                    tempTxt.text = "${main.temp}${getString(R.string.degreeCelsius)}"
+                    TempInfoTxt.text =
+                        "${main.tempMin}${getString(R.string.degree)}    " + "${main.tempMax}${getString(R.string.degree)}"
+                    //Include
+                    weatherLay.apply {
+                        feelCountTxt.text = "${main.feelsLike}${getString(R.string.degreeCelsius)}"
+                        pressureCountTxt.text = "${main.humidity}%"
+                    }
+                }
+                //Wind
+                data.wind?.let { wind ->
+                    weatherLay.windCountTxt.text = "${wind.speed} ${getString(R.string.km_s)}"
+                }
 
             }
         }
