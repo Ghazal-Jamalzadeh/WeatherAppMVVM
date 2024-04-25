2878baa3eb8eef9dd3af21f9dde694cb5881ee10
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-25 19:17:30 +0430

    #428 InfoBottomSheet : args

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/info/InfoFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/info/InfoFragment.kt
@@ -3,17 +3,31 @@ package com.jmzd.ghazal.weatherappmvvm.ui.info
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.jmzd.ghazal.weatherappmvvm.databinding.FragmentInfoBinding
 import com.jmzd.ghazal.weatherappmvvm.utils.base.BaseBottomSheetFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class InfoFragment : BaseBottomSheetFragment<FragmentInfoBinding>() {
+
+    //binding
     override val bindingInflater: (inflater: LayoutInflater) -> FragmentInfoBinding
         get() = FragmentInfoBinding::inflate
 
+    //args
+    private val args by navArgs<InfoFragmentArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //init views
+        binding.apply {
+            //Args
+            args.data.let { data ->
+
+            }
+        }
     }
 
 
