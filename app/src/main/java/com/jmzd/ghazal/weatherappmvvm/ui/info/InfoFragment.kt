7a670b0271fa1663d11ba8367520b3f5da46e6d0
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-26 12:20:40 +0430

    #429 Pollution : observer

--- app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/info/InfoFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/weatherappmvvm/ui/info/InfoFragment.kt
@@ -1,16 +1,26 @@
 package com.jmzd.ghazal.weatherappmvvm.ui.info
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.jmzd.ghazal.weatherappmvvm.R
 import com.jmzd.ghazal.weatherappmvvm.databinding.FragmentInfoBinding
 import com.jmzd.ghazal.weatherappmvvm.utils.BASE_URL_IMAGE
 import com.jmzd.ghazal.weatherappmvvm.utils.PNG_IMAGE
 import com.jmzd.ghazal.weatherappmvvm.utils.base.BaseBottomSheetFragment
+import com.jmzd.ghazal.weatherappmvvm.utils.changeVisibility
 import com.jmzd.ghazal.weatherappmvvm.utils.loadImage
+import com.jmzd.ghazal.weatherappmvvm.utils.network.NetworkRequest
+import com.jmzd.ghazal.weatherappmvvm.utils.showSnackBar
+import com.jmzd.ghazal.weatherappmvvm.viewmodel.InfoViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class InfoFragment : BaseBottomSheetFragment<FragmentInfoBinding>() {
@@ -18,6 +28,9 @@ class InfoFragment : BaseBottomSheetFragment<FragmentInfoBinding>() {
     //binding
     override val bindingInflater: (inflater: LayoutInflater) -> FragmentInfoBinding
         get() = FragmentInfoBinding::inflate
+
+    //viewModel
+    private val viewModel by viewModels<InfoViewModel>()
 
     //args
     private val args by navArgs<InfoFragmentArgs>()
@@ -29,6 +42,14 @@ class InfoFragment : BaseBottomSheetFragment<FragmentInfoBinding>() {
         binding.apply {
             //Args
             args.data.let { data ->
+
+                //Call api
+                lifecycleScope.launch {
+                    delay(500)
+                    data.coord?.let { coord ->
+                        viewModel.getPollution(coord.lat!!, coord.lon!!)
+                    }
+                }
 
                 //Weather
                 data.weather?.let { weather ->
@@ -58,6 +79,66 @@ class InfoFragment : BaseBottomSheetFragment<FragmentInfoBinding>() {
                     weatherLay.windCountTxt.text = "${wind.speed} ${getString(R.string.km_s)}"
                 }
 
+            }
+        }
+
+        //observers
+        loadPollutionData()
+    }
+
+    private fun loadPollutionData() {
+        binding.apply {
+            viewModel.pollutionLiveData.observe(viewLifecycleOwner) { response ->
+                when (response) {
+                    is NetworkRequest.Loading -> {
+                        loading.changeVisibility(true, pollutionCard)
+                    }
+
+                    is NetworkRequest.Success -> {
+                        loading.changeVisibility(false, pollutionCard)
+                        response.data?.let { data ->
+                            data.list?.let { list ->
+                                if (list.isNotEmpty()) {
+                                    list[0].let { myData ->
+//                                        pollutionLay.apply {
+//                                            //Image
+//                                            pollutionIconImg.apply {
+//                                                setImageResource(pollutionIcon(myData.main))
+//                                                //Tint
+//                                                imageTintList = ColorStateList.valueOf(
+//                                                    ContextCompat.getColor(requireContext(), pollutionColors(myData.main))
+//                                                )
+//                                            }
+//                                            //Info
+//                                            pollutionInfoTxt.apply {
+//                                                text = pollutionMessage(myData.main)
+//                                                setTextColor(
+//                                                    ContextCompat.getColor(requireContext(), pollutionColors(myData.main))
+//                                                )
+//                                            }
+//                                            //Shadow
+//                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+//                                                pollutionCard.apply {
+//                                                    outlineAmbientShadowColor =
+//                                                        ContextCompat.getColor(requireContext(), pollutionColors(myData.main))
+//                                                    outlineSpotShadowColor =
+//                                                        ContextCompat.getColor(requireContext(), pollutionColors(myData.main))
+//                                                }
+//                                            }
+//                                            //Pollution
+//                                            initRecyclerView(fillPollutionData(myData.components))
+//                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    is NetworkRequest.Error -> {
+                        loading.changeVisibility(false, pollutionCard)
+                        root.showSnackBar(response.error!!)
+                    }
+                }
             }
         }
     }
